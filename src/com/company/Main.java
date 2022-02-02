9eec9f331c784d5762a785d207fadb749d46acf1
Rushnan-Faria <98732391+Rushnan-Faria@users.noreply.github.com>
2022-02-02 15:40:15 +0600

    added main.java

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Select the type of consumable you want to add:\n1.Book,\n2.Series,\n3.Movie.");
+    }
+}
